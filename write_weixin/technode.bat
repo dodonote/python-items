246033b6e951dd38cadfb6507541e4551560673a
呆呆 <dodonote@sina.com>
2018-08-30 12:19:12 +0800

    bat

---
+++ write_weixin/technode.bat
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*-
+import os
+import time
+
+command3 = 'python technode.py'
+
+os.chdir('E:\\jetbrains\\python-items\\write_weixin')
+os.system(command3)
+print '******************'
+print '******************'
+print 'success'
